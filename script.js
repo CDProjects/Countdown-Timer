a18e578451b15a46944342af3dc5f13e95877a04
Kiwicase <caseydentprojects@gmail.com>
2023-03-28 13:55:20 +0300

    JS, CSS and HTML all updated to resolve issues.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const daysEl = document.getElementById('days');
-const hoursEl = document.getElementById('hours');
-const minsEl = document.getElementById('mins');
-const secondsEl = document.getElementById('seconds');
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
-
-const newYears = '1 Jan 2024';
+const newYears = "1 Jan 2021";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
@@ -18,14 +17,16 @@ function countdown() {
     const seconds = Math.floor(totalSeconds) % 60;
 
     daysEl.innerHTML = days;
-    hoursEl.innerHTML = hours;
-    minsEl.innerHTML = mins;
-    secondsEl.innerHTML = seconds;
-
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
 
-// inital call
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
 countdown();
 
 setInterval(countdown, 1000);
-
